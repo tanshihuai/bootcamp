52c30fa91d429e920586981fe84f98f8db605e6c
shihuai <tanshihuai@gmail.com>
2022-05-11 22:01:44 +0800

    form html created

---
+++ register/views.py
@@ -0,0 +1,10 @@
+from re import U
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'register/register.html', {'form': form})
