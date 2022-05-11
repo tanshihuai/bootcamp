09ea7b41e59b6677b435b2584361bb14bdf28314
shihuai <tanshihuai@gmail.com>
2022-05-12 01:17:36 +0800

    implemented RegisterForm inherited from UserCreationForm

--- register/views.py
+++ register/views.py
@@ -1,10 +1,17 @@
 from re import U
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/home")
+
+    else:
+        form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
