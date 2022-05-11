51b4f849c76ef91eb52afa4b150abcf625caf0f7
shihuai <tanshihuai@gmail.com>
2022-05-12 01:23:45 +0800

    implemented crispy forms

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'register'
 ]
 
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
